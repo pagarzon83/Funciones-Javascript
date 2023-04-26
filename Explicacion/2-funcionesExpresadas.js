c839468fb84cbfcfc3088d57264f7c3b415d54be
Ana Garcia <docenteagarcia@udistrital.edu.co>
2023-04-26 16:16:38 -0500

    funcion

--- Explicacion/2-funcionesExpresadas.js
+++ Explicacion/2-funcionesExpresadas.js
@@ -2,7 +2,7 @@
 Las funciones expresadas hacen parte del ámbito concreto del programa, es decir, estas funciones no existen hasta que la expresión que las genera es ejecutada. Se consideran una buena practica de programación para tener el código ordenado*/
 
 console.log("--------------------FUNCIONES EXPRESADAS---------------------------------")
-const funcionExpresada = function{
+const funcionExpresada = function(){
     return console.log("Esta es una función expresada!");
 }
 
